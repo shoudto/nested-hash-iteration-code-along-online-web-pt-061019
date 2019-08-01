ba905da5e3c15dc2b3cfb171b1ba9294a3e37069
Oscar Shoudt <shoudto@gmail.com>
2019-08-01 20:12:50 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, data|
     if person == "Freddy Mercury"
-      data.each do ||
+      data.each do |example, sample|
 
     end
   end
